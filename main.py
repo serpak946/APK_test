4fea5d4022ba66c63cbcb3f589537683533d5dfb
serpak946 <63670242+serpak946@users.noreply.github.com>
2021-07-27 02:26:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ from kivy.uix.textinput import TextInput
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.core.window import Window
-import requests
-from bs4 import BeautifulSoup
 
 
 
@@ -160,8 +158,6 @@ class MyApp(App):
         self.EURL.color = (0.83,0.96,0.81,1)
         wn.add_widget(self.EURL)
 
-
-
         return wn
 
     def code(self):
@@ -174,104 +170,6 @@ class MyApp(App):
             self.UAHL.text = 'UAH = 0.0'
             self.MDLL.text = 'MDL = 0.0'
             self.EURL.text = 'EUR = 0.0'
-        else:
-            SUMRUP = RUP1 + (USD1 * a8) + (RUB1 * a2) + (MDL1*a19) + (UAH1 * a3) + (EUR1*a17)
-            SUMRUB = RUB1 + (RUP1 / a1) + (USD1 * a9) + (EUR1 * a15) + ((MDL1*a19)/a1) + ((UAH1*a3)/a1)
-            SUMUSD = USD1 + (RUP1 / a7) + (RUB1 / a10) + (EUR1 * a13) + ((MDL1*a19)/a7) + ((UAH1*a3)/a7)
-            SUMUAH = UAH1 + ((RUP1 + (USD1 * a8) + (RUB1 * a2) + (MDL1 * a19) + (EUR1 * a17)) / a4)
-            SUMMDL = MDL1 + ((RUP1 + (USD1 * a8) + (RUB1 * a2) + (UAH1 * a3) + (EUR1 * a17)) / a5)
-            SUMEUR = EUR1 + (RUP1 / a18) + (RUB1 / a16) + (USD1 / a14) + ((MDL1 * a19) / a18) + ((UAH1 * a3) / a18)
-            self.RUPL.text = 'RUP = '+str(round(SUMRUP, 2))
-            self.USDL.text = 'USD = ' + str(round(SUMUSD, 2))
-            self.RUBL.text = 'RUB = ' + str(round(SUMRUB, 2))
-            self.UAHL.text = 'UAH = ' + str(round(SUMUAH, 2))
-            self.MDLL.text = 'MDL = ' + str(round(SUMMDL, 2))
-            self.EURL.text = 'EUR = ' + str(round(SUMEUR, 2))
-
-
-URL = 'https://www.agroprombank.com/info/rates.html'
-HEADERS = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0",
-           "Accept-Encoding": "*",
-           "Connection": "keep-alive"
-           }
-
-
-def get_html(url, params=None):
-    '''Заходим на сайт'''
-    r = requests.get(url, headers=HEADERS, params=params)
-    return r
-
-
-def get_content(html):
-    """"Из всего кода отделяем стоимость и берём 55 и 56 строчку, где находится RUB/RUP"""
-    global pok
-    global prod
-    global pok1
-    global prod1
-    global prod2
-    global items
-    soup = BeautifulSoup(html, 'html.parser')
-    items = soup.findAll('td', class_='td')
-    pok = (items[55].get_text())
-    pok1 = float(pok)
-    prod = (items[56].get_text())
-    prod1 = float(prod)
-
-
-def parse():
-    """Проверяем, подключились ли мы к сайту"""
-    html = get_html(URL)
-    if html.status_code == 200:
-        get_content(html.text)
-    else:
-        print('Error')
-
-
-parse()
-# print(items)
-a1 = (items[56].get_text())
-a2 = (items[55].get_text())
-a3 = (items[52].get_text())
-a4 = (items[53].get_text())
-a5 = (items[50].get_text())
-a6 = (items[49].get_text())
-a7 = (items[38].get_text())
-a8 = (items[37].get_text())
-a9 = (items[34].get_text())
-a10 = (items[35].get_text())
-a11 = (items[32].get_text())
-a12 = (items[31].get_text())
-a13 = (items[40].get_text())
-a14 = (items[41].get_text())
-a15 = (items[43].get_text())
-a16 = (items[44].get_text())
-a17 = (items[46].get_text())
-a18 = (items[47].get_text())
-a19 = (items[49].get_text())
-a20 = (items[50].get_text())
-
-# print(a1,a2,a3,a4,a5,a6,a7,a8,a9,a10)
-
-a1=float(a1)
-a2=float(a2)
-a3=float(a3)
-a4=float(a4)
-a5=float(a5)
-a6=float(a6)
-a7=float(a7)
-a8=float(a8)
-a9=float(a9)
-a10=float(a10)
-a11=float(a11)
-a12=float(a12)
-a13=float(a13)
-a14=float(a14)
-a15=float(a15)
-a16=float(a16)
-a17=float(a17)
-a18=float(a18)
-a19=float(a19)
-a20=float(a20)
 
 k=0
 
